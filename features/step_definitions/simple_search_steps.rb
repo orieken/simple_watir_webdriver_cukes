a135895cb6403bb5b8e5a552f45af9c6f75e230d
orieken <oriekenjr@gmail.com>
2012-03-02 22:02:13 -0500

    Scenario: Search for Eloquent Ruby fully passing

--- features/step_definitions/simple_search_steps.rb
+++ features/step_definitions/simple_search_steps.rb
@@ -19,8 +19,9 @@ When /^I follow the "([^"]*)" link$/ do |the_link_i_want_to_click|
   @browser.link(:text => the_link_i_want_to_click).click
 end
 
-Then /^I should see the title "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Then /^I should see the title "([^"]*)"$/ do |omg_the_title_is_free|
+#it should be something more meaningful like expected_title but it is free for us not a wholelot of code in this step
+  @browser.title.should eql(omg_the_title_is_free)
 end
 
 Then /^I should not see the title "([^"]*)"$/ do |arg1|
